ebd86e073167e042f49c8d8e76e529d348ac576d
pavlina <102150683+Pavlina-G@users.noreply.github.com>
2024-05-25 17:39:33 +0300

    Update Templates, Adding initial controllers and routes

--- workshop-movie-magic/src/config/express.js
+++ workshop-movie-magic/src/config/express.js
@@ -2,7 +2,7 @@ const { urlencoded, static: expressStatic } = require('express');
 
 function configExpress(app) {
     app.use(urlencoded({ extended: true }));
-    app.use('static', expressStatic('static'));
+    app.use('/static', expressStatic('static'));
 }
 
 module.exports = { configExpress };
--- workshop-movie-magic/src/config/routes.js
+++ workshop-movie-magic/src/config/routes.js
@@ -1,7 +1,21 @@
 const {Router} = require('express');
 
+const { notFound } = require('../controllers/404');
+const { home, details, search } = require('../controllers/catalog');
+const { about } = require('../controllers/about');
+const { create } = require('../controllers/movie');
+
 const router = Router();
 
 // TORO adding routes
+
+router.get('/', home);
+router.get('/about', about);
+router.get('/search', search);
+router.get('/create', create);  //TODO add createGet and createPost
+
+router.get('/details', details);  //TODO add :id
+
+router.get('*', notFound)
 
 module.exports = {router};
