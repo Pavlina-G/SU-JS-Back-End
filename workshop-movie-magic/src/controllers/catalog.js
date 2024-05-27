a43350c27a1507d4939e5e8ecbfc184c4ef61762
pavlina <102150683+Pavlina-G@users.noreply.github.com>
2024-05-27 16:30:39 +0300

    Adding Document title

--- workshop-movie-magic/src/controllers/catalog.js
+++ workshop-movie-magic/src/controllers/catalog.js
@@ -1,10 +1,12 @@
 const { getAllMovies, getMovieById } = require("../services/movie");
+const { findMovieAdded } = require("../utils/util");
+
 
 module.exports = {
     home: async (req, res) => {
         const movies = await getAllMovies();
 
-        res.render('home', { movies });
+        res.render('home', { movies, title: 'Catalog' });
     },
     details: async (req, res) => {
         const id = req.params.id;
@@ -15,10 +17,61 @@ module.exports = {
             return;
         }
         movie.starRating = '&#x2605;'.repeat(movie.rating);
-        
-        res.render('details', {movie});
+
+        res.render('details', { movie, title: movie.title.slice(0, 20) });
     },
-    search: (req, res) => {
-        res.render('search');
+    search: async (req, res) => {
+        let movies = await getAllMovies();
+        let results = [];
+        let foundMovie = true;
+        let initialState = true;
+        let filteredMovies = []
+
+
+        if (Object.keys(req.query).length == 0) {
+            results = movies;
+        } else {
+            initialState = false;
+        }
+        if (Object.values(req.query).every(v => v == '')) {
+            foundMovie = false;
+            results = [];
+
+        } else {
+            const { title, genre, year } = req.query;
+
+
+            if (title !== '') {
+                filteredMovies = (movies.filter(m => m.title.toLowerCase().includes(title.toLowerCase())));
+
+                if (filteredMovies.length > 0) {
+                    results = filteredMovies;
+                    filteredMovies = []
+                }
+            }
+            if (genre !== '') {
+
+                filteredMovies = movies.filter(m => m.genre.toLowerCase().includes(genre.toLowerCase()));
+
+                if (filteredMovies.length > 0) {
+                    results = findMovieAdded(results, filteredMovies)
+                    filteredMovies = []
+                }
+            }
+            if (year !== '') {
+                filteredMovies = movies.filter(m => m.year === Number(year));
+
+                if (filteredMovies.length > 0) {
+                    results = findMovieAdded(results, filteredMovies)
+                    filteredMovies = []
+                }
+            }
+            if (results.length === 0) {
+                foundMovie = false;
+            }
+
+        }
+
+        res.render('search', { title: 'Search', movies, results, foundMovie, initialState });
     }
 }
--- workshop-movie-magic/src/controllers/movie.js
+++ workshop-movie-magic/src/controllers/movie.js
@@ -1,6 +1,31 @@
+const { createMovie } = require('../services/movie')
+const pageTitle = 'Create Movie';
+
 module.exports = {
+
     createGet: (req, res) => {
-        res.render('create')
+        res.render('create', {title:pageTitle})
+    },
+    createPost: async (req, res) => {
+        const errors = {
+            title: !req.body.title,
+            genre: !req.body.genre,
+            director: !req.body.director,
+            year: !req.body.year,
+            imageURL: !req.body.imageURL,
+            rating: !req.body.rating,
+            description: !req.body.description
+        };
+
+        // console.log(errors);
+
+        if (Object.values(errors).includes(true)) {
+            res.render('create', { movie: req.body, errors});
+            return;
+        }
+
+        const result = await createMovie(req.body);
+
+        res.redirect('/details/' + result.id);
     }
-    //TODO add create POST
 }
