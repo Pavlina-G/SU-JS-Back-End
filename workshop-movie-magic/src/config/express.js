894340f99ae8c8241d561e0b62eb6b3a1b0d9b86
pavlina <102150683+Pavlina-G@users.noreply.github.com>
2024-06-10 15:57:52 +0300

    Workshop part3 - adding session

--- workshop-movie-magic/src/config/express.js
+++ workshop-movie-magic/src/config/express.js
@@ -1,9 +1,11 @@
 const { urlencoded, static: expressStatic } = require('express');
 const cookieParser = require('cookie-parser');
+const { session } = require('../middlewares/session');
 require('dotenv').config();
 
 function configExpress(app) {
     app.use(cookieParser(process.env.SECRET));
+    app.use(session());
     app.use(urlencoded({ extended: true }));
     app.use('/static', expressStatic('static'));
 }
